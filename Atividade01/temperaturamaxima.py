a1275de3817b873911302a7e9fc3174fecd5694d
Lucas Luan <lucas.c.luan1@gmail.com>
2026-04-01 19:57:17 -0300

    Mais atividades

--- Atividade01/temperaturamaxima.py
+++ Atividade01/temperaturamaxima.py
@@ -1,1 +1,27 @@
 from mrjob.job import MRJob
+
+
+class MaxTempEstacao(MRJob):
+    def mapper(self, _, line):
+        try:
+
+            if ';' in line:
+                campos = line.split(';')
+            else:
+                campos = line.split(',')
+
+            if len(campos) >= 2:
+                estacao = campos[0].strip()
+                temp = float(campos[1].strip())
+                yield estacao, temp
+        except ValueError:
+            pass
+
+    def reducer(self, estacao, temps):
+
+        yield estacao, max(temps)
+
+
+if __name__ == '__main__':
+    MaxTempEstacao.run()
+
